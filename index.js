8d788b2fc2d0ad2d723075d6820f3f0d0713c579
shafiqbd1st <shafiqbd1st@gmail.com>
2025-01-22 12:44:00 +0600

    add top six most booked classes

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ async function run() {
         })
         app.patch('/class-update/:id', verifyToken, async (req, res) => {
             const id = req.params.id;
-            console.log('update', id)
+            // console.log('update', id)
             const query = { _id: new ObjectId(id) }
             const update = {
                 $inc: { count: 1 },
